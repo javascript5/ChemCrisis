e345f2f779c1153f450354e80289487125bd7809
javascript5 <javascript5@hotmail.com>
2019-01-28 11:25:32 +0700

    [Add] Initial files :tada:

---
+++ MobileApplocation/NSCChemCrisis/app/src/main/java/com/chemcrisis/nsc/nscchemcrisis/MainActivity.java
@@ -0,0 +1,112 @@
+package com.chemcrisis.nsc.nscchemcrisis;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.Gradient;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+import com.google.maps.android.heatmaps.WeightedLatLng;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    private void addHeatMap() {
+        List<LatLng> list = null;
+
+        // Get the data: latitude/longitude positions of police stations.
+        try {
+            list = readItems(R.raw.dataset);
+        } catch (JSONException e) {
+            Log.e("TEST", "Problem reading list of locations.");
+        }
+
+        int[] colors = {
+                Color.GREEN,    // green(0-50)
+                Color.YELLOW,    // yellow(51-100)
+                Color.rgb(255,165,0), //Orange(101-150)
+                Color.RED,              //red(151-200)
+                Color.rgb(153,50,204), //dark orchid(201-300)
+                Color.rgb(165,42,42) //brown(301-500)
+        };
+
+        float[] startpoints = {
+                0.1F, 0.2F, 0.3F, 0.4F, 0.6F, 1.0F
+        };
+
+        Gradient gradient = new Gradient(colors, startpoints);
+        ArrayList<WeightedLatLng> data = new ArrayList<WeightedLatLng>();
+
+        data.add(new WeightedLatLng(new LatLng(23.424076, 53.847818), 2));
+
+        // Create a heat map tile provider, passing it the latlngs of the police stations.
+        if (mProvider == null) {
+            mProvider = new HeatmapTileProvider.Builder().weightedData(data).build();
+
+
+            mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+            mProvider.setRadius(100);
+
+            mOverlay.clearTileCache();
+        }
+
+    }
+
+
+    private ArrayList<LatLng> readItems(int resource) throws JSONException {
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+        InputStream inputStream = getResources().openRawResource(resource);
+        String json = new Scanner(inputStream).useDelimiter("\\A").next();
+        JSONArray array = new JSONArray(json);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.getJSONObject(i);
+            double lat = object.getDouble("lat");
+            double lng = object.getDouble("lng");
+            list.add(new LatLng(lat, lng));
+        }
+        return list;
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng( 23.424076, 	53.847818);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(6), 500, null);
+        addHeatMap();
+    }
+}
